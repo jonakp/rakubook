c4be97f6b7b2163fa3d66101e51a904be9f3ef21
Takahiro Masui <mst2253@gmail.com>
2019-03-16 20:33:02 +0900

    DEV-23 add user_id & filtering by user_id to Reading

--- db/migrate/20190309055622_create_readings.rb
+++ db/migrate/20190309055622_create_readings.rb
@@ -1,8 +1,9 @@
+# 読書履歴マスタの作成
 class CreateReadings < ActiveRecord::Migration[5.2]
   def change
     create_table :readings do |t|
-      t.string :name
-      t.integer :price
+      t.string :name,   comment: '書籍名'
+      t.integer :price, comment: '価格'
 
       t.timestamps
     end
---
+++ db/migrate/20190316104329_add_user_ref_to_reading.rb
@@ -0,0 +1,5 @@
+class AddUserRefToReading < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :readings, :user, foreign_key: true
+  end
+end
